6f64aa6bb3344e86adfac9bd2b4bddddd99c796e
Felipe Moura Madureira <44149509+madfelps@users.noreply.github.com>
2022-02-21 12:09:26 -0300

    Create simple scanner

---
+++ main.py
@@ -0,0 +1,13 @@
+while(1):
+    #Scanning our nearby devices using bluetooth
+    nearby_devices = bluetooth.discover_devices(lookup_names=True)
+
+    print("number of devices: ", len(nearby_devices))
+    payload = update_device(len(nearby_devices))
+
+    #Sending our payload to influxDB
+    client.write_points(payload)
+
+    for addr, name in nearby_devices:
+        print("address: ", addr)
+        print("name ", name)
